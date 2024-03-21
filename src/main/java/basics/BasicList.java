b113595f7f8e74a6bb049941114bb5d945d9cf8b
leonardo <leofreitas.engineer@gmail.com>
2024-03-21 18:46:27 -0300

    learn the basics of array list

---
+++ src/main/java/basics/BasicList.java
@@ -0,0 +1,16 @@
+package basics;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class BasicList {
+    public static void main(String[] args) {
+
+    }
+
+    public static void declaration() {
+        List<String> alunos = new ArrayList<>();
+        alunos.add("João");
+        alunos.add("Maria");
+    }
+}
